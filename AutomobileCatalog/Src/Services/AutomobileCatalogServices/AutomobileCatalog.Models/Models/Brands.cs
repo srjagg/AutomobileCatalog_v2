e7e276a4bec47129a8cf13e676b583d8c0650820
Johan García <jaggandreyj1@gmail.com>
2024-05-29 10:13:38 -0500

    INSERT - Creación de modelos (Brands y Cars)

---
+++ AutomobileCatalog/Src/Services/AutomobileCatalogServices/AutomobileCatalog.Models/Models/Brands.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomobileCatalog.Models.Models
+{
+    public class Brands
+    {
+        public int Id { get; set; }
+        public string? Nombre { get; set; }
+
+        public ICollection<Cars>? Cars { get; set; }
+    }
+}
---
+++ AutomobileCatalog/Src/Services/AutomobileCatalogServices/AutomobileCatalog.Models/Models/Cars.cs
@@ -0,0 +1,14 @@
+﻿namespace AutomobileCatalog.Models.Models
+{
+    public class Cars
+    {
+        public int Id { get; set; }
+        public string Modelo { get; set; }
+        public string Descripción { get; set; }
+        public decimal? Precio { get; set; }
+        public decimal? Kilometraje { get; set; }
+
+        public int BrandId { get; set; }
+        public Brands Brands { get; set; }
+    }
+}
